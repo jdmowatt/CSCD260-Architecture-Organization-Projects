1853cc77897b7656f5a124852cdbcfa48400eac5
Jake Mowatt <64564259+jdmowatt@users.noreply.github.com>
2026-04-19 22:09:14 -0700

    Update Q1.s

--- Lab 1/Q1.s
+++ Lab 1/Q1.s
@@ -6,7 +6,7 @@ _start:
 	ADD R0, R0, R1	@ R0 <-- R0 + R1
 	
 	MOV R1, #150	@ Move decimal 150 to R1
-	ADD R0, R0, R1	@ R0 <-- R0 + R1
+	ADDS R0, R0, R1	@ R0 <-- R0 + R1
 	
 	MOV R1, #200	@ Move decimal 200 to R1
-	ADD R0, R0, R1	@ R0 <-- R0 + R1
+	ADDS R0, R0, R1	@ R0 <-- R0 + R1
